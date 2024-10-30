df6ffe6569af6196ade3bb02562935cd2d3a1a8a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-10-31 00:17:41 +0100

    Add session type configuration and enable server-side sessions

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,16 @@ from flask import (
   Flask, request, jsonify
 )
 from flask_cors import CORS
+from flask_session import Session
 from config import Config
 from api.extensions import db
 from dotenv import load_dotenv
 from api.models.article import Article
+from api.models.user import User
+from werkzeug.security import (
+  generate_password_hash, check_password_hash
+)
+from redis import Redis
 
 load_dotenv()
 def create_app(config_object=Config):
@@ -14,11 +20,14 @@ def create_app(config_object=Config):
   # enable CORS
   CORS(app)
 
+  # Enable server-side sessions
+  Session(app)
+
   # Initailize database
   db.init_app(app)
-
+  app.config['SESSION_REDIS'] = Redis(host='localhost', port=6379)
   with app.app_context():
-    db.drop_all()
+    # db.drop_all()
     db.create_all()
 
 
@@ -36,6 +45,35 @@ def create_app(config_object=Config):
 
       return jsonify({'message': 'Liked post'})
 
+    return jsonify({'message': 'Not authorized'}), 403
+
+
+  @app.route('/register', methods=['POST'])
+  def register():
+    data = request.get_json()
+    user = User.query.filter(User.username == data['username'] or User.email == data['email']).first()
+    # check if a user with same userame or email already exists
+    if user:
+      return jsonify({'message': 'User already exists'}), 409
+
+    try:
+      # create a new user
+      new_user = User(email=data['email'], password_hash=generate_password_hash(data['password']), username=data['username'])
+
+      db.session.add(new_user)
+      db.session.commit()
+      return jsonify({'message': 'User registered successfully'}), 201
+    except Exception as e:
+      db.session.rollback()
+      return jsonify({'message': 'An error occured while creating user', 'error': str(e)}), 500
+
+
+  @app.route('/login', methods=['POST'])
+  def login():
+    data = request.get_json()
+    # check if email and password are empty
+    if data['email'] and data['password']:
+      user = User.query.filter(User.email)
     
 
   return app
