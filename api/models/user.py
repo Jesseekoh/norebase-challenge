98bb9126d8bca2c4818d19a6cb3f44abfbc9b312
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-10-31 09:48:02 +0100

    Refactor User model and add relationship with Article model

--- api/models/user.py
+++ api/models/user.py
@@ -1,7 +1,8 @@
 from api.extensions import db
 from sqlalchemy import (
-  Column, String, UUID, ForeignKey
+  Column, String, UUID, ForeignKey, Table
 )
+from sqlalchemy.orm import relationship
 from uuid import uuid4
 
 
@@ -11,3 +12,12 @@ class User(db.Model):
   username = Column(String, nullable=False, unique=True)
   email = Column(String, nullable=False, unique=True)
   password_hash = Column(String, nullable=False)
+  liked_articles = relationship('Article', secondary='user_article_likes', back_populates='liked_by_users')
+  
+
+user_article_likes = Table('user_article_likes', db.Model.metadata, 
+  Column('user_id', UUID(as_uuid=True), ForeignKey('users.id'),               
+      primary_key=True),
+  Column('article_id', UUID(as_uuid=True), ForeignKey('articles.id'), 
+      primary_key=True)
+)
