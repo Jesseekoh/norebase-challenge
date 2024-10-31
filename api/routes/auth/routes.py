71f7237bed0bfe2d035c1a81e0f128032d9624ef
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-10-31 01:40:17 +0100

    Add authentication routes for user registration and login

---
+++ api/routes/auth/routes.py
@@ -0,0 +1,45 @@
+from flask import (
+  request, jsonify, session
+)
+from api.models.user import User
+from api.extensions import db
+
+from api.routes.auth import bp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+@bp.route('/register', methods=['POST'])
+def register():
+  data = request.get_json()
+  user = User.query.filter(User.username == data['username'] or User.email == data['email']).first()
+  # check if a user with same userame or email already exists
+  if user:
+    return jsonify({'message': 'User already exists'}), 409
+
+  try:
+    # create a new user
+    new_user = User(email=data['email'], password_hash=generate_password_hash(data['password']), username=data['username'])
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully'}), 201
+  except Exception as e:
+    db.session.rollback()
+    return jsonify({'message': 'An error occured while creating user', 'error': str(e)}), 500
+
+
+@bp.route('/login', methods=['POST'])
+def login():
+  data = request.get_json()
+  # check if email and password are empty
+  if data['email'] and data['password']:
+    user = User.query.filter(User.email == data['email']).first()
+    if user:
+      # check if password is correct
+      if check_password_hash(user.password_hash, data['password']):
+        session['user_id'] = user.id
+        return jsonify({'message': "You've loged in successfully"}), 200
+      else:
+        return jsonify({'message': 'Wrong password'}), 401
+    
+    return jsonify({'message': 'User does not exist'}), 404
+  return jsonify({'message': 'Email and password are required'}), 400
