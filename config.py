613e9d25a3af87b64ebd4cc8abbf13f4a707fdf9
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-10-31 11:28:30 +0100

    Refactor config.py to set default values for environment variables

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-  SESSION_TYPE = os.environ.get('SESSION_TYPE')
-  SESSION_COOKIE_HTTPONLY= os.environ.get('SESSION_COOKIE_HTTPONLY')
-  SESSION_COOKIE_SECURE= os.environ.get('SESSION_COOKIE_SECURE')
-  SESSION_PERMANENT = os.environ.get('SESSION_PERMANENT')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///default.db')
+  SESSION_TYPE = os.environ.get('SESSION_TYPE', 'redis')
+  SESSION_COOKIE_HTTPONLY= os.environ.get('SESSION_COOKIE_HTTPONLY', 'True')
+  SESSION_COOKIE_SECURE= os.environ.get('SESSION_COOKIE_SECURE', 'True')
+  SESSION_PERMANENT = os.environ.get('SESSION_PERMANENT', 'False')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False  # To suppress a warning from SQLAlchemy
